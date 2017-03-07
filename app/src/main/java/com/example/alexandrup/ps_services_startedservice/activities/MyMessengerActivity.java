6d00738218539067601903fdc69ff7c9f9632c92
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-03-07 09:41:49 +0200

    Commit 7 - IPC using messages

--- app/src/main/java/com/example/alexandrup/ps_services_startedservice/activities/MyMessengerActivity.java
+++ app/src/main/java/com/example/alexandrup/ps_services_startedservice/activities/MyMessengerActivity.java
@@ -1,9 +1,14 @@
 package com.example.alexandrup.ps_services_startedservice.activities;
 
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,24 +16,55 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.alexandrup.ps_services_startedservice.R;
+import com.example.alexandrup.ps_services_startedservice.services.MyMessengerService;
 
 /**
  * Created by alexandrup on 3/6/2017.
  */
 
+//make sure you bind service before you ADD !!!
 public class MyMessengerActivity extends AppCompatActivity {
 
     boolean mIsBound = false;
     private TextView txvResult;
 
+    private Messenger mService = null;	// To send data to Service
+
+    private class IncomingResponseHandler extends Handler { // To receive data back from Service
+
+        @Override
+        public void handleMessage(Message msgFromService) {
+
+            switch (msgFromService.what) {
+
+                case 87:
+
+                    Bundle bundle = msgFromService.getData();
+                    int result = bundle.getInt("result", 0);
+
+                    txvResult.setText("Result: " + result);
+                    break;
+
+                default:
+                    super.handleMessage(msgFromService);
+            }
+        }
+    }
+
+    // To receive data back from Service
+    private Messenger incomingMessenger = new Messenger(new IncomingResponseHandler());
+
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            mService = new Messenger(service);
             mIsBound = true;
         }
 
         @Override
-        public void onServiceDisconnected(ComponentName componentName) {
+        public void onServiceDisconnected(ComponentName name) {
+
             mIsBound = false;
         }
     };
@@ -37,7 +73,6 @@ public class MyMessengerActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messenger);
-
         txvResult = (TextView) findViewById(R.id.txvResult);
     }
 
@@ -48,11 +83,35 @@ public class MyMessengerActivity extends AppCompatActivity {
 
         int num1 = Integer.valueOf(etNumOne.getText().toString());
         int num2 = Integer.valueOf(etNumTwo.getText().toString());
+
+        Message msgToService = Message.obtain(null, 43);
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("numOne", num1);
+        bundle.putInt("numTwo", num2);
+
+        msgToService.setData(bundle);
+        msgToService.replyTo = incomingMessenger;
+
+        try {
+            mService.send(msgToService);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     public void bindService(View view) {
+
+        Intent intent = new Intent(this, MyMessengerService.class);
+        bindService(intent, mConnection, BIND_AUTO_CREATE);
     }
 
     public void unbindService(View view) {
+
+        if (mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
     }
 }
+
